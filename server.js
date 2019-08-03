9de3e0854ff1611c4edab18382d36623e0326de3
Matt Groberg <grobergmg@gmail.com>
2019-08-02 19:39:45 -0700

    remove part 05 code

--- server.js
+++ server.js
@@ -22,29 +22,4 @@ app.get('/users/:id',function(req,res){
 	console.log(req.params.id)
 })
 
-app.post('/login',function(req,res){
-	// Typically passwords are encrypted using something like bcrypt before sending to database
-	const username=req.body.username;
-	const password=req.body.password;
-
-	// This should come from the database
-	const mockUsername="billyTheKid";
-	const mockPassword="superSecret";
-
-	if (username===mockUsername && password===mockPassword){
-		// In practice, use JSON web token sign method here to make an encrypted token
-		res.json({
-			success: true,
-			message: 'password and username match!',
-			token: 'encrypted token goes here'
-		})
-	} else {
-		res.json({
-			success: false,
-			message: 'password and username do not match'
-		})
-	}
-
-})
-
 app.listen(8000,function(){console.log('server is listening')})
