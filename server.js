cb27ec042e0119efad6504bd5e798ed302b7ce2f
Matt Groberg <grobergmg@gmail.com>
2019-08-02 19:49:02 -0700

    remove part 03 code

--- server.js
+++ server.js
@@ -1,17 +1,4 @@
 const express = require('express');
 const app = express();
 
-const mockUserData=[
-	{name:'Mark'},
-	{name:'Jill'}
-]
-
-app.get('/users',function(req,res){
-	res.json({
-		success: true,
-		message: 'successfully got users. Nice!',
-		users: mockUserData
-	})
-})
-
 app.listen(8000,function(){console.log('server is listening')})
