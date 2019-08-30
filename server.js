6f899e9217ffd6f456962d3db1982d6d71415331
Matt Groberg <grobergmg@gmail.com>
2019-08-30 09:38:28 -0700

    return something

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ app.get('/users',function(req,res){
 // colons are used as variables that be viewed in the params
 app.get('/users/:id',function(req,res){
 	console.log(req.params.id)
+	res.json({
+		success: true,
+		message: 'got one user',
+		user: req.params.id
+	})
 })
 
 app.listen(8000,function(){console.log('server is listening')})
