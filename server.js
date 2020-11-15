cfc85eef61212653f67cbcb70092b85babbcaf3c
Eddie Jaoude <eddie@jaoudestudios.com>
2020-11-15 22:40:47 +0000

    feat: mock login

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
 const mockUserData = [
     { id: 1, name: "Mark" },
-    { id: 2, name: "Jill" }
+    { id: 2, name: "Jill" },
 ];
 app.get("/users/:id", function (req, res) {
     res.json({
@@ -21,6 +24,27 @@ app.get("/users", function (req, res) {
         id: req.params.id,
         users: mockUserData,
     });
+});
+
+app.post("/login", function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockUsername = "billyTheKid";
+    const mockPassword = "superSecret";
+
+    if (username === mockUsername && password === mockPassword) {
+        res.json({
+        success: true,
+        message: "password and username match!",
+        token: "encrypted token goes here",
+        });
+    } else {
+        res.json({
+        success: false,
+        message: "password and username do not match",
+        });
+    }
 });
 
 app.listen(8000, function () {
