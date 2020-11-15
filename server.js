a9f486fd0a753154f78daffa392885f61c56abf4
Eddie Jaoude <eddie@jaoudestudios.com>
2020-11-15 22:30:31 +0000

    feat: get resource by id

--- server.js
+++ server.js
@@ -2,13 +2,23 @@ const express = require("express");
 const app = express();
 
 const mockUserData = [
-    { name: "Mark" },
-    { name: "Jill" }
+    { id: 1, name: "Mark" },
+    { id: 2, name: "Jill" }
 ];
+app.get("/users/:id", function (req, res) {
+    res.json({
+        success: true,
+        message: "successfully got users. Nice!",
+        id: req.params.id,
+        user: mockUserData.find((user) => user.id == req.params.id),
+    });
+});
+
 app.get("/users", function (req, res) {
     res.json({
         success: true,
         message: "successfully got users. Nice!",
+        id: req.params.id,
         users: mockUserData,
     });
 });
