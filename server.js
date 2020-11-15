38e2990fef2ae9d56e62dca5af0338784ab8d0f7
Eddie Jaoude <eddie@jaoudestudios.com>
2020-11-15 22:22:39 +0000

    feat: users endpoint

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.listen(8000,function(){
-    console.log("server is running")
-})
+const mockUserData = [
+    { name: "Mark" },
+    { name: "Jill" }
+];
+app.get("/users", function (req, res) {
+    res.json({
+        success: true,
+        message: "successfully got users. Nice!",
+        users: mockUserData,
+    });
+});
+
+app.listen(8000, function () {
+    console.log("server is running");
+});
