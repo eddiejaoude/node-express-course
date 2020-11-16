8ed5880dbd1f7c2fe45a2051114ac87038ef47a9
Saravanan raghul <saravananraghul1820@gmail.com>
2020-11-16 22:28:57 +0530

    Added if-else block and changed the message and status

--- server.js
+++ server.js
@@ -1,52 +1,75 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-const mockUserData = [
-    { id: 1, name: "Mark" },
-    { id: 2, name: "Jill" },
+let function_message = '';
+let test_status = '';
+const message = 'successfully got users. Nice!';
+const status = true;
+
+const mockUserData = [{
+        id: 1,
+        name: 'Mark'
+    },
+    {
+        id: 2,
+        name: 'Jill'
+    }
 ];
-app.get("/users/:id", function (req, res) {
+
+app.get('/users/:id', function (req, res) {
+    if (mockUserData.find((user) => user.id == req.params.id)) {
+        res.json({
+            status: status,
+            message: message,
+            user: mockUserData.find((user) => user.id == req.params.id)
+        });
+    } else {
+        test_status = false;
+        id = req.params.id;
+        function_message = `Sorry user doesn't exist for the id ${id}`;
+        res.json({
+            status: test_status,
+            message: function_message
+        });
+    }
+});
+
+app.get('/users', function (req, res) {
     res.json({
-        success: true,
-        message: "successfully got users. Nice!",
+        status: status,
+        message: message,
         id: req.params.id,
-        user: mockUserData.find((user) => user.id == req.params.id),
+        users: mockUserData
     });
 });
 
-app.get("/users", function (req, res) {
-    res.json({
-        success: true,
-        message: "successfully got users. Nice!",
-        id: req.params.id,
-        users: mockUserData,
-    });
-});
-
-app.post("/login", function (req, res) {
+app.post('/login', function (req, res) {
     const username = req.body.username;
     const password = req.body.password;
 
-    const mockUsername = "billyTheKid";
-    const mockPassword = "superSecret";
+    const mockUsername = 'billyTheKid';
+    const mockPassword = 'superSecret';
 
     if (username === mockUsername && password === mockPassword) {
+        function_message = 'password and username match!';
         res.json({
-        success: true,
-        message: "password and username match!",
-        token: "encrypted token goes here",
+            status: status,
+            message: function_message,
+            token: 'encrypted token goes here'
         });
     } else {
+        test_status = false;
+        function_message = ' username and password do not match';
         res.json({
-        success: false,
-        message: "password and username do not match",
+            status: test_status,
+            message: function_message
         });
     }
 });
 
 app.listen(8000, function () {
-    console.log("server is running");
+    console.log('server is running');
 });
