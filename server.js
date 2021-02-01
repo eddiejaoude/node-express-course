5a140e3e1c0b2cc1916f477632a83c2e2a9edf5d
Yagnesh Parekh <yagneshparekh@rocketmail.com>
2021-02-01 17:21:02 -0500

    uninstall body-parser pacakage as express 4.16+ has inbuild json parser

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
+
+app.use(express.json());
 
 const mockUserData = [{
         id: 1,
